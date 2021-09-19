2f4d1340d570eeef921ee023faeff1e200575cf8
twfb <twfb29@yahoo.com>
2021-09-19 11:58:08 -0400

    user model

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username:
+        {
+            type: String,
+            Unique: true,
+            required: true,
+            trimmed: true
+        },
+        email:
+        {
+            type: String,
+            Unique: true,
+            required: true,
+            match: [/.+\@.+\..+/]
+
+        },
+        thoughts:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        friends:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    });
+
+    userSchema.virtual('friendCount').get(function(){
+        return this.friends.length
+    });
+
+    const User = model('User', userSchema);
+
+    module.exports = User;
+    
