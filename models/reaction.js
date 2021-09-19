813ac81ed62f707178b5506d90f8e4d7e5fe1f5e
twfb <twfb29@yahoo.com>
2021-09-19 16:27:29 -0400

    finishing up the models

--- models/reaction.js
+++ models/reaction.js
@@ -1,5 +1,6 @@
 const {Schema, Types} = require('mongoose');
 const moment = require('moment');
+const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema(
     {
--- models/thought.js
+++ models/thought.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./reaction');
 const moment = require('moment');
+const dateFormat = require('../utils/dateFormat');
 
 const thoughtSchema = new Schema (
     {
