b7c3592a560390521c778c35e090606f2b8d5c80
twfb <twfb29@yahoo.com>
2021-09-19 11:26:49 -0400

    reactionSchema

--- models/reaction.js
+++ models/reaction.js
@@ -1,2 +1,32 @@
 const {Schema, Types} = require('mongoose');
 const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => Types.ObjectId()
+    },
+    reactionBody:{
+        type: String ,
+        required: true,
+        max: 280
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+    },
+    {
+    toJSON:{
+        getters: true
+    },
+    id: false
+});
+
+module.exports = reactionSchema;
